d92eb59e830981e77abafa9c3bf1a2623f5b1594
theti <daniellikhachev@gmail.com>
2024-12-21 14:41:54 +0800

    minor

--- src/components/Sphere.vue
+++ src/components/Sphere.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div>
+  <div class="video-container">
     <video autoplay loop muted playsinline>
       <source src="/3d-glassy-spheric-vortex-of-pastel-light.webm" type="video/webm" />
       Ваш браузер не поддерживает воспроизведение видео.
@@ -12,5 +12,11 @@
 </template>
 
 <style scoped>
-
+.video-container {
+  position: relative;
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
