7d52282f963c4e16ecdf772813a0cbec3c68f618
iumag <iumag@yandex.ru>
2024-12-22 19:06:21 +0800

    Sphere webm for windows

--- src/components/Sphere.vue
+++ src/components/Sphere.vue
@@ -1,11 +1,13 @@
 <script setup>
 const isAndroid = /android/i.test(navigator.userAgent);
+const isWindows = /windows|win32/i.test(navigator.userAgent);
+let isWebm = isAndroid || isWindows
 </script>
 
 <template>
   <div class="video-container">
     <video autoplay loop muted playsinline>
-      <source v-if="isAndroid" src="/3d-glassy-spheric-vortex-of-pastel-light.webm" type="video/mp4"/>
+      <source v-if="isWebm" src="/3d-glassy-spheric-vortex-of-pastel-light.webm" type="video/mp4"/>
       <source v-else src="/3d-glassy-spheric-vortex-of-pastel-light.mov" type="video/mp4"/>
       Ваш браузер не поддерживает воспроизведение видео.
     </video>
